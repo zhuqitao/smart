991c99b1e068a05482b2188743fa70ce92cb9138
zhuqitao <1362335135@qq.com>
2018-04-10 21:03:13 +0800

    添加用户页面 账单页面

--- pages/add-user/add-user.js
+++ pages/add-user/add-user.js
@@ -1,23 +1,16 @@
-// pages/edit/edit.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
-  // 点击 添加人员
-  addUser: function () {
-    wx.navigateTo({
-      url: '../add-user/add-user',
-    })
+
   }
 })

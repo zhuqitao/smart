db441c664871c29d8941b4115b895b3fc52b991b
zhuqitao <1362335135@qq.com>
2018-04-24 21:20:05 +0800

    首页跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,46 @@ const app = getApp()
 
 Page({
   data: {
-    userList: []
+    userInfo: {},
+    userList: [],
+    loading: true
   },
   //事件处理函数
 
+  
+
   onLoad: function () {
+    console.log(app.globalData)
+    wx.getUserInfo({
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+      }
+    })
+    wx.showLoading({
+      title: 'aa',
+      mask: true
+    })
     app.login().then(res => {
       app.ajax('xcx/user/getCheckWorker', 'POST').then(res => {
-        console.log(res)
-          this.setData({
-            userList: res.data.body.checkWorkJoindata
-          })
+        wx.hideLoading()
+        this.setData({
+          userList: res.data.body.checkWorkJoindata,
+          loading: false
+        })
       })
+    })
+  },
+
+  onShow: function () {
+    console.log('a')
+  },
+
+  // 添加出勤人员
+  editStaff: function () {
+    wx.switchTab({
+      url: '../edit/edit',
     })
   },
 })
